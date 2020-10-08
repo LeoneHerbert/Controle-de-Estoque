a4cef58a83b09a4abb3a845ced90359b25af00ad
LeoneHerbert <herbleopin@gmail.com>
2020-10-08 17:52:23 -0300

    feat(HeaderLocationEvent): Criar classe HeaderLocationEvent

---
+++ src/main/java/com/rns/testes/java/controller/event/HeaderLocationEvent.java
@@ -0,0 +1,24 @@
+package com.rns.testes.java.controller.event;
+
+import org.springframework.context.ApplicationEvent;
+import javax.servlet.http.HttpServletResponse;
+
+public class HeaderLocationEvent extends ApplicationEvent {
+
+    private final HttpServletResponse response;
+    private final Integer id;
+
+    public HeaderLocationEvent(Object source, HttpServletResponse response, Integer id) {
+        super(source);
+        this.response = response;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public HttpServletResponse getResponse() {
+        return response;
+    }
+}
