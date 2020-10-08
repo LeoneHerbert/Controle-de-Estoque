56d5ef372f7b24180ebcb509a3e36eaf1c0a9c92
LeoneHerbert <herbleopin@gmail.com>
2020-10-07 21:42:07 -0300

    refactor(Estoque): Adicionar métodos de adição e baixa de um produto no estoque

--- src/main/java/com/rns/testes/java/model/Estoque.java
+++ src/main/java/com/rns/testes/java/model/Estoque.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Entity
 @Table(name = "ESTOQUE")
@@ -17,4 +18,20 @@ public class Estoque {
     @Min(0)
     @Column(name = "quantidade_produtos")
     private Integer quantidadeProdutos;
+
+    public void adicionaNoEstoque(@Min(1) Integer quantidade) {
+        this.setQuantidadeProdutos(this.getQuantidadeProdutos() + quantidade);
+    }
+
+    public void baixaNoEstoque(@Positive Integer quantidade)  {
+        final int novaQuantidade = this.getQuantidadeProdutos() - quantidade;
+
+        if (novaQuantidade < 0) {
+            throw new IllegalArgumentException
+                    ("Não há disponibilidade no estoque de "
+                            + quantidade + " itens do produto " + id.getProduto().getNome() + "."
+                            + "Temos disponível apenas " + this.quantidadeProdutos + "itens" );
+        }
+        this.setQuantidadeProdutos(novaQuantidade );
+    }
 }
